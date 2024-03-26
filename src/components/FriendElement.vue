d129a95dc4151100600e5bcf2df1ba54e3d1ad99
trong hieu <hieu745233@gmail.com>
2024-03-26 23:05:47 +0700

    first commit

---
+++ src/components/FriendElement.vue
@@ -0,0 +1,16 @@
+<template>
+    <button
+        class="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
+        <div
+        class="flex items-center justify-center h-8 w-8 bg-gray-200 rounded-full">
+        M
+        </div>
+        <div class="ml-2 text-sm font-semibold"><slot name="name"></slot></div>
+        <div
+        class="flex items-center justify-center ml-auto text-xs text-white bg-red-500 h-4 w-4 rounded leading-none">
+        <slot name="number_message"></slot>
+        </div>
+    </button>
+</template>
+
+  
---
+++ src/views/ChatBox.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import SidebarChat from '@/components/SidebarChat.vue'
+import ChatRoom from '@/components/ChatRoom.vue'
+</script>
+
+<template>
+<!-- component -->
+<div class="flex h-screen antialiased text-gray-800">
+    <div class="flex flex-row h-full w-full overflow-x-hidden">
+      <!-- Sidebar Chat -->
+      <SidebarChat></SidebarChat>
+
+      <!-- Chat Room -->
+      <ChatRoom></ChatRoom>
+    </div>
+  </div>
+</template>
